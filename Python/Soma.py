3bd50cd20d221956ab21cc07c8135a79fdcb9971
Peters <pedrojulio.barreto@gmail.com>
2025-03-06 16:06:25 -0300

    troca de nome

--- Python/Soma.py
+++
@@ -1,9 +0,0 @@
-# Parte para botar os números
-num1 = int(input("Digite o primeiro número que deseja somar: "))
-num2 = int(input("Digite o segundo número que deseja somar: "))
-
-# Parte da soma
-soma = num1 + num2
-
-# Print da soma
-print(f"A soma de {num1} + {num2} é {soma}")
